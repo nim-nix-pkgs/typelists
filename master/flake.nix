7868ef527d81f0de084f3c389978b7870a5dad46
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:47 -0300

    chore: re-index ../nimpkgs/t/typelists

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Typelists in Nim'';
+  description = ''Type lists'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-typelists-master.flake = false;
   inputs.src-typelists-master.owner = "yglukhov";
-  inputs.src-typelists-master.ref   = "refs/heads/master";
+  inputs.src-typelists-master.ref   = "master";
   inputs.src-typelists-master.repo  = "typelists";
   inputs.src-typelists-master.type  = "github";
   
